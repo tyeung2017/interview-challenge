28be8b587399ca324886e1d007d913e1af77b864
leo <leo42us@gmail.com>
2020-09-28 23:15:42 +0100

    update Basic Card so that it has the remove button for type of card in the Menu area

--- src/client/components/BasicCard.js
+++ src/client/components/BasicCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BasicCard = ({ name, dietaries, id }) => (
+const BasicCard = ({
+  name, dietaries, id, withButton, handleRemove,
+}) => (
   <li className="item">
     <h2>{name}</h2>
     <p>
       {dietaries.map(dietary => <span className="dietary" key={`${id}-${dietary}`}>{dietary}</span>)}
     </p>
+    {withButton && <button className="remove-item" type="button" onClick={() => handleRemove(id)}>x</button>}
   </li>
 );
 
@@ -16,4 +19,11 @@ BasicCard.propTypes = {
   name: PropTypes.string.isRequired,
   dietaries: PropTypes.arrayOf(PropTypes.string).isRequired,
   id: PropTypes.number.isRequired,
+  withButton: PropTypes.bool,
+  handleRemove: PropTypes.func,
+};
+
+BasicCard.defaultProps = {
+  withButton: false,
+  handleRemove: () => {},
 };
