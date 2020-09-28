ebd8c68998b1b81f28c0b6d42ea2f135da51b1d8
leo <leo42us@gmail.com>
2020-09-28 22:49:06 +0100

    refactor out the card component in left sidebar with dummy props

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Header from './components/Header.js';
+import Header from './components/Header';
+import BasicCard from './components/BasicCard';
 import './App.css';
 
 class App extends Component {
@@ -20,62 +21,7 @@ class App extends Component {
                 <input className="form-control" placeholder="Name" />
               </div>
               <ul className="item-picker">
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                </li>
+                <BasicCard name="temp" dietaries={['v', 've']} id={1001} />
               </ul>
             </div>
             <div className="col-8">
