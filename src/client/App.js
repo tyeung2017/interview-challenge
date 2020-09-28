966886e2ba5b5c2e5721d02bec0e98e643032139
leo <leo42us@gmail.com>
2020-09-28 23:16:12 +0100

    refactor card in Menu area with updated Basic Card

--- src/client/App.js
+++ src/client/App.js
@@ -27,42 +27,7 @@ class App extends Component {
             <div className="col-8">
               <h2>Menu preview</h2>
               <ul className="menu-preview">
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
+                <BasicCard name="temp" dietaries={['v', 've']} id={1001} withButton handleRemove={() => {}} />
               </ul>
             </div>
           </div>
